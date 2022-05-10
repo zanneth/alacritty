6349b6f72281a7f6beed53418c7a4eab9b4728e5
Lorenzo Tucci <yamabiiko@protonmail.com>
2022-05-11 00:03:02 +0200

    implementing text blinking closes #2365

--- alacritty_terminal/src/event.rs
+++ alacritty_terminal/src/event.rs
@@ -44,6 +44,9 @@ pub enum Event {
     /// Cursor blinking state has changed.
     CursorBlinkingChange,
 
+    /// Cursor blinking state has changed.
+    BlinkingChange(bool),
+
     /// New terminal content available.
     Wakeup,
 
@@ -67,6 +70,7 @@ impl Debug for Event {
             Event::PtyWrite(text) => write!(f, "PtyWrite({text})"),
             Event::Title(title) => write!(f, "Title({title})"),
             Event::CursorBlinkingChange => write!(f, "CursorBlinkingChange"),
+            Event::BlinkingChange(status) => write!(f, "BlinkingChange({})", status),
             Event::MouseCursorDirty => write!(f, "MouseCursorDirty"),
             Event::ResetTitle => write!(f, "ResetTitle"),
             Event::Wakeup => write!(f, "Wakeup"),
